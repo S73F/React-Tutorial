486f7ab053ba9147ce032b2e1e89c103b5668dd4
Stefano Cherio <stefano.cherio@edu.itspiemonte.it>
2025-06-21 19:02:46 +0200

    changed layout

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
+import { Box } from "@mui/material";
 
 export const Layout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Box
+        height={{
+          xs: "calc(100vh - 56px)",
+          sm: "calc(100vh - 64px)",
+          md: "calc(100vh - 68.5px)",
+        }}
+        padding={3}
+      >
+        <Outlet />
+      </Box>
     </>
   );
 };
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,18 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 export const Home = () => {
   return (
-    <Stack
-      justifyContent="center"
-      alignItems="center"
-      height={{
-        xs: "calc(100vh - 56px)",
-        sm: "calc(100vh - 64px)",
-        md: "calc(100vh - 68.5px)",
-      }}
-    ></Stack>
+    <Stack justifyContent={"center"} textAlign={"center"} height={"100%"}>
+      <Typography variant="h3" component="h1" gutterBottom>
+        Welcome to the React + TypeScript + MUI template!
+      </Typography>
+      <Typography variant="h6" component="p" gutterBottom>
+        This template is designed to help you get started quickly with a modern
+        React application using TypeScript and Material-UI.
+      </Typography>
+      <Typography variant="body1" component="p" gutterBottom>
+        Explore the code, customize it to your needs, and build something great!
+      </Typography>
+    </Stack>
   );
 };
