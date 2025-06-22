34fbc55b036e65c2336a0e5a2217506304c0c2a9
Stefano Cherio <stefano.cherio@edu.itspiemonte.it>
2025-06-22 15:56:25 +0200

    added Prenotazioni.tsx route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Home } from "./routes/Home";
 import { Layout } from "./component/Layout";
 import { Attrezzature } from "./routes/Attrezzature";
+import { Prenotazioni } from "./routes/Prenotazioni";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/equipments" element={<Attrezzature />} />
-          <Route path="/bookings" element={<Home />} />
+          <Route path="/bookings" element={<Prenotazioni />} />
         </Route>
       </Routes>
     </BrowserRouter>
