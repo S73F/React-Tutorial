e2af7930ae4865791ca376ec3b8a3d3d43d15ac7
Stefano Cherio <stefano.cherio@edu.itspiemonte.it>
2025-06-26 11:54:49 +0200

    added token to booking

--- src/services/sendBooking.api.ts
+++ src/services/sendBooking.api.ts
@@ -1,9 +1,19 @@
 import { DEFAULT_URL } from "../constants";
 
-export const sendBooking = async (id: number, duration: number) => {
+export const sendBooking = async (
+  id: number,
+  duration: number,
+  token?: string
+) => {
+  const headers: HeadersInit = { "Content-Type": "application/json" };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
   const response = await fetch(`${DEFAULT_URL}/api/equipment/${id}/book`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: headers,
     body: JSON.stringify({ duration }),
   });
 
